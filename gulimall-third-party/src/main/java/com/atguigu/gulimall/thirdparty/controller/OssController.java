ece73ce85739c63f3c201bcdc83816c3e3556e7f
liu-jia-jun <3142467441@qq.com>
2022-08-13 23:11:33 +0800

    update

---
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
@@ -0,0 +1,10 @@
+package com.atguigu.gulimall.thirdparty.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 刘佳俊
+ */
+@RestController
+public class OssController {
+}
